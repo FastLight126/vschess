4fd4a0ff15dc4569914652ae83e1949b1c1b647e
FastLight126 <fastlight@fastlight.cn>
2018-06-11 00:49:44 +0800

    优化帮助信息

--- vschess/develop/module/module.help.js
+++ vschess/develop/module/module.help.js
@@ -1,12 +1,20 @@
 // 创建帮助区域
 fn.createHelp = function(){
 	var _this = this;
+	var helpDetail = this.options.help.replace(/#quickStepOffsetRound#/g, this._.quickStepOffset / 2).replace(/#quickStepOffset#/g, this._.quickStepOffset);
 	this.helpArea = $('<div class="vschess-help-area"></div>');
-	this.helpArea.html(this.options.help);
+	this.helpArea.html('<div class="vschess-help-area-detail">' + helpDetail + '</div>');
 	this.DOM.append(this.helpArea);
 	this.helpAreaClose = $('<input type="button" class="vschess-button vschess-help-close" value="关 闭" />');
 	this.helpAreaClose.bind(this.options.click, function(){ _this.hideHelpArea(); });
 	this.helpArea.append(this.helpAreaClose);
+	return this;
+};
+
+// 刷新帮助信息
+fn.refreshHelp = function(){
+	var helpDetail = this.options.help.replace(/#quickStepOffsetRound#/g, this._.quickStepOffset / 2).replace(/#quickStepOffset#/g, this._.quickStepOffset);
+	this.helpArea.children(".vschess-help-area-detail").html(helpDetail);
 	return this;
 };
 
